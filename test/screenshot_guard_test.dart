69e3a16b77cd3268151dae9e01c5f9c9a408298a
Daniel <thecodedaniel@gmail.com>
2024-12-23 23:57:41 +0100

    docs: plugin documentation updates

--- test/screenshot_guard_test.dart
+++ test/screenshot_guard_test.dart
@@ -7,6 +7,14 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockScreenshotGuardPlatform
     with MockPlatformInterfaceMixin
     implements ScreenshotGuardPlatform {
+  // Mock for enableSecureFlag method
+  @override
+  Future<void> enableSecureFlag(bool enable) async {
+    // Implement the mock logic if necessary (no-op for now)
+    return;
+  }
+
+  // Mock for getPlatformVersion method
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
 }
@@ -24,6 +32,18 @@ void main() {
     MockScreenshotGuardPlatform fakePlatform = MockScreenshotGuardPlatform();
     ScreenshotGuardPlatform.instance = fakePlatform;
 
+    // Test getPlatformVersion
     expect(await screenshotGuardPlugin.getPlatformVersion(), '42');
+  });
+
+  test('enableSecureFlag', () async {
+    ScreenshotGuard screenshotGuardPlugin = ScreenshotGuard();
+    MockScreenshotGuardPlatform fakePlatform = MockScreenshotGuardPlatform();
+    ScreenshotGuardPlatform.instance = fakePlatform;
+
+    // Test enableSecureFlag method
+    await screenshotGuardPlugin.enableSecureFlag(true);
+
+    // Add any necessary verification or assertions if needed
   });
 }
